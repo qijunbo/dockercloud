4804258dce2ab405aca6fc015eba6203e1b6206c
qijunbo <junboqi@foxmail.com>
2017-09-06 18:09:37 +0800

    产品容器化方案.

--- auto/createcontainer.sh
+++ auto/createcontainer.sh
@@ -6,17 +6,17 @@ if [ -z "${name}" ]; then
    exit -1 ;
 fi
 
-mkdir -p  /root/mysql/${name}/data
-mkdir -p  /root/mysql/${name}/conf
-mkdir -p  /root/mysql/${name}/initsql
+mkdir -p  /home/docker/mysql/${name}/data
+mkdir -p  /home/docker/mysql/${name}/conf
+mkdir -p  /home/docker/mysql/${name}/initsql
 
-cp -n conf/*  /root/mysql/${name}/conf
-cp -n initsql/*.sql  /root/mysql/${name}/initsql
+cp -n conf/*  /home/docker/mysql/${name}/conf
+cp -n initsql/*.sql  /home/docker/mysql/${name}/initsql
 
 docker run --name mysql${name} -e MYSQL_ROOT_PASSWORD=sunway123# -d -P \
-    -v /root/mysql/${name}/data:/var/lib/mysql  \
-    -v /root/mysql/${name}/initsql:/docker-entrypoint-initdb.d \
-    -v /root/mysql/${name}/conf:/etc/mysql/conf.d   mysql
+    -v /home/docker/mysql/${name}/data:/var/lib/mysql  \
+    -v /home/docker/mysql/${name}/initsql:/docker-entrypoint-initdb.d \
+    -v /home/docker/mysql/${name}/conf:/etc/mysql/conf.d   mysql
 
 docker run --name lims${name} -d -P sunway/lims:1
 
