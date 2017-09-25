be34a7a0ea2a1068bb4bead1d8241528e8a44f94
qijunbo <junboqi@foxmail.com>
2017-09-25 15:36:22 +0800

    new container start form qijunbo/mysql

--- docker/createcontainer.sh
+++ docker/createcontainer.sh
@@ -21,6 +21,6 @@ docker run --name mysql${name} -e MYSQL_ROOT_PASSWORD=sunway123# -d -P \
     -v /home/docker/mysql/${name}/data:/var/lib/mysql  \
     -v /home/docker/mysql/${name}/initsql:/docker-entrypoint-initdb.d \
     -v /home/docker/mysql/${name}/conf:/etc/mysql/conf.d \
-          mysql --character-set-server=utf8 --collation-server=utf8_general_ci 
+         qijunbo/mysql --character-set-server=utf8 --collation-server=utf8_general_ci 
 logger "Mysql container(mysql${name}) started at: `date`"
 
